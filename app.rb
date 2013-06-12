546425d6324db9412531ea422be490c7a88b8339
Sky Helpcentre <skyhelpcentre@gmail.com>
2013-06-12 11:58:54 +0100

    RD: Moving the javascript files into a lib folder
    The javascript files for Omniture are the focus of this project, not the Sinatra
    app. The Sinatra app will now load these files upon request from the lib folder.

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
-require 'bundler'                                                                                             
-Bundler.require :default                                                                                      
+require 'bundler'
+Bundler.require :default
 
 class App < Sinatra::Base
   get '/' do
-    erb :index 
+    erb :index
+  end
+
+  get '/js/:filename' do
+    send_file File.join('lib', params[:filename])
   end
 end
