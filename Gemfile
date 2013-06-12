7275f2458a8a928a26d1fa1c62b492afb6134383
Sky Helpcentre <skyhelpcentre@gmail.com>
2013-06-12 14:26:20 +0100

    RD: Changes to instructions

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'capybara'
 gem "minitest-capybara"
 gem 'poltergeist'
 gem 'sinatra'
+gem 'sass'
 gem 'addressable'
 gem 'pry'
 gem 'aws-sdk'
--- app.rb
+++ app.rb
@@ -6,7 +6,22 @@ class App < Sinatra::Base
     erb :index
   end
 
-  get '/js/:filename' do
+
+  # ASSETS
+  get '/js/lib/:filename' do
     send_file File.join('lib', params[:filename])
+  end
+  get '/js/:filename' do
+    send_file File.join('views', 'js', params[:filename])
+  end
+
+  get '/css/:filename' do
+    filename = File.join('views','styles', params[:filename])
+    if File.exists?(filename)
+      send_file filename
+    else
+      filename = params[:filename].gsub(/\.css$/,'')
+      scss "styles/#{filename}".to_sym, :style => :expanded
+    end
   end
 end
