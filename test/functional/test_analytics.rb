320db20fe952125c7957fa7145f23aff99a64cc4
Sky Helpcentre <skyhelpcentre@gmail.com>
2013-06-12 10:41:57 +0100

    RD: Whitespace clean up [ci skip]

--- test/functional/test_analytics.rb
+++ test/functional/test_analytics.rb
@@ -1,11 +1,11 @@
 require_relative '../test_helper.rb'
 
 def http_request regex
-    page.driver.network_traffic.reverse.find { |req| req.url =~ regex }
+  page.driver.network_traffic.reverse.find { |req| req.url =~ regex }
 end
 
 def last_sitecat_request
-    Addressable::URI.parse http_request(/metrics.sky.com/).url
+  Addressable::URI.parse http_request(/metrics.sky.com/).url
 end
 
 def tracked
@@ -13,13 +13,13 @@ def tracked
 end
 
 def sitecat_mapping
-    {
-       'section' => 'c27',
-       'step 3 section' => 'c31',
-       'contentType' => 'c20',
-       'page' => 'pageName',
-       'event' => 'events'
-    }
+  {
+    'section' => 'c27',
+    'step 3 section' => 'c31',
+    'contentType' => 'c20',
+    'page' => 'pageName',
+    'event' => 'events'
+  }
 end
 
 class AnalyticsTest < AcceptanceTest
@@ -29,11 +29,11 @@ class AnalyticsTest < AcceptanceTest
   end
 
   it "Tracks page view" do
-     tracked['events'].must_include 'event1'
+    tracked['events'].must_include 'event1'
   end
 
   it "Tracks the correct page name" do
-     tracked['pageName'].must_equal 'sky/portal/mysky/Analytics demo page'
+    tracked['pageName'].must_equal 'sky/portal/mysky/Analytics demo page'
   end
 
   it "Tracks a link being clicked" do
