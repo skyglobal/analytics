3d50d456e545e89f92a3e39f6101e4da7af45b01
Sky Helpcentre <skyhelpcentre@gmail.com>
2013-06-12 10:34:00 +0100

    RD: Tests
    - Move setup into a test helper
    - Create a Rake task to run functional tests

--- test/functional/test_analytics.rb
+++ test/functional/test_analytics.rb
@@ -1,41 +1,26 @@
-require 'minitest/autorun'
-require 'minitest-capybara'
-require 'capybara/poltergeist'
-require_relative '../app'
+require_relative '../test_helper.rb'
 
-class AcceptanceTest < MiniTest::Spec
-  include Capybara::RSpecMatchers
-  include Capybara::DSL
-  Capybara.default_driver = :poltergeist
-  Capybara.app = App
+def http_request regex
+    page.driver.network_traffic.reverse.find { |req| req.url =~ regex }
 end
 
-
-require 'addressable/uri'                                             
-                                                                      
-def http_request regex                                                
-    page.driver.network_traffic.reverse.find { |req| req.url =~ regex } 
-end                                                                   
-                                                                      
-def last_sitecat_request                                              
-    Addressable::URI.parse http_request(/metrics.sky.com/).url          
-end                                      
+def last_sitecat_request
+    Addressable::URI.parse http_request(/metrics.sky.com/).url
+end
 
 def tracked
   last_sitecat_request.query_values
 end
-                                                                      
-def sitecat_mapping                                                   
-    {                                                                   
-       'section' => 'c27',                                               
-       'step 3 section' => 'c31',                                        
-       'contentType' => 'c20',                                           
-       'page' => 'pageName',                                             
-       'event' => 'events'                                               
-    }                                                                   
-end                                                                   
 
-require 'pry'
+def sitecat_mapping
+    {
+       'section' => 'c27',
+       'step 3 section' => 'c31',
+       'contentType' => 'c20',
+       'page' => 'pageName',
+       'event' => 'events'
+    }
+end
 
 class AnalyticsTest < AcceptanceTest
 
@@ -55,5 +40,4 @@ class AnalyticsTest < AcceptanceTest
     click_link "Standard anchor link"
   end
 
-  
 end
---
+++ test/test_helper.rb
@@ -0,0 +1,15 @@
+ENV['RACK_ENV'] = 'test'
+require 'minitest/autorun'
+require 'minitest-capybara'
+require 'capybara/poltergeist'
+require 'rack/test'
+require 'addressable/uri'
+
+require File.expand_path '../../app.rb', __FILE__
+
+class AcceptanceTest < MiniTest::Spec
+  include Capybara::RSpecMatchers
+  include Capybara::DSL
+  Capybara.default_driver = :poltergeist
+  Capybara.app = App
+end
