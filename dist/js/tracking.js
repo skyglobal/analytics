676b6f5f015535ae0accc648639fb1abd3ba8701
chandler <dl-skycom@chandler.com>
2013-10-10 12:41:21 +0100

    RD:PM: refactor - move plugins into separate files. getPreviousValue removed.

--- dist/js/tracking.js
+++ dist/js/tracking.js
@@ -1073,21 +1073,6 @@ toolkit.omniture = (function(config, utils, h26,
                 +"++]=l.substring(0,i);l=l.substring(i+d.length);}return a");
 
 
-
-
-            /*
-             * Plugin: getPreviousValue v1.0 - return previous value of designated
-             *   variable (requires split utility)
-             */
-            s.getPreviousValue=new Function("v","c","el",""
-                +"var s=this,t=new Date,i,j,r='';t.setTime(t.getTime()+1800000);if(el"
-                +"){if(s.events){i=s.split(el,',');j=s.split(s.events,',');for(x in i"
-                +"){for(y in j){if(i[x]==j[y]){if(s.c_r(c)) r=s.c_r(c);v?s.c_w(c,v,t)"
-                +":s.c_w(c,'no value',t);return r}}}}}else{if(s.c_r(c)) r=s.c_r(c);v?"
-                +"s.c_w(c,v,t):s.c_w(c,'no value',t);return r}");
-
-
-
             s.httpsSearch = function(A){var pp = "";if(A.indexOf("www.google.") != -1){;if(document.referrer.indexOf("q=&")!=-1){pp = "google";}}if(pp == ""){pp = "na";}return pp;}
             /* Top 130 - Grouped */
 
