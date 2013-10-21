318229a121ada1d7b06ccf309c60a9daebf65b13
chandler <dl-skycom@chandler.com>
2013-10-21 17:44:55 +0100

    PM|RD: Remove node_modules from repo
    Resolves #2

---
+++ dist/js/wiki.js
@@ -0,0 +1,101 @@
+
+if (typeof _wiki==='undefined') _wiki={};
+_wiki.setup = (function(){
+
+    function bindEvents(){
+        $('.toggle-code-example').on('click', toggleCodeExamples);
+        $('#btn_ajax_event').on('click', ajaxPageView);
+        $('#btn-page-load-var').on('click', customVarPageView);
+        $('#search-results-pageView').on('click', searchResultsPageView);
+        updateCustomVariableValues();
+        manualTrigger();
+    }
+
+    function toggleCodeExamples(){
+        var $toggler = $(this);
+        var $example = $('#' + $toggler.attr('for'));
+        if ($example.hasClass('open')){
+            $toggler.removeClass('open');
+            $example.removeClass('open');
+        } else {
+            $toggler.addClass('open');
+            $example.addClass('open');
+        }
+    }
+
+    function updateCustomVariableValues(){
+            $('[data-tracking-variable=how_about_pina_coladas]').attr('data-tracking-value',$('input[name=radio_field]:checked').val());
+            $('[data-tracking-variable=drink]').attr('data-tracking-value',$('#input_field').val());
+            $('#input_field').on('blur', function(e){
+                $('[data-tracking-variable=drink]').attr('data-tracking-value',$(this).val());
+            });
+            $('#radio-input input').on('click', function(e){
+                $('[data-tracking-variable=how_about_pina_coladas]').attr('data-tracking-value',$(this).val());
+            });
+            $('#text-input button, #radio-input button').on('click', function(e){
+                e.preventDefault();
+            });
+    }
+
+    function ajaxPageView(e){
+        e.preventDefault();
+        analytics.pageView({
+            site: 'global',
+            section: 'skyglobal/analytics',
+            account: 'bskybglobaldev',
+            page: 'Analytics single page apps demo page',
+            customEvents: [
+                {'ajax_happened': {event: 101, onPageLoad: true}}
+            ],
+            customVariables: [
+                {'colour': {var: 71 }},
+                {'drink': {'var': 72 }},
+                {'how_about_pina_coladas': {'var': 73 }}
+            ]
+        });
+    }
+
+    function searchResultsPageView(e){
+        e.preventDefault();
+        analytics.pageView({
+            site: 'global',
+            section: 'skyglobal/analytics',
+            account: 'bskybglobaldev',
+            page: 'Analytics single page apps demo page',
+            loadVariables: { 'searchResults': 0, 'searchType':'weather', 'searchTerm': 'london' }
+        });
+    }
+
+    function customVarPageView(e){
+        e.preventDefault();
+        analytics.pageView({
+            site: 'global',
+            section: 'skyglobal/analytics',
+            account: 'bskybglobaldev',
+            page: 'Analytics single page apps demo page',
+            customVariables: [
+                {'how_about_pina_coladas': {'var': 73, 'value': 'my val on load', 'onPageLoad': true }}
+            ]
+        });
+    }
+
+    function manualTrigger(){
+        $('#manualTrackingOff').on('click', function(){
+            $('#manualTrackingOff').trigger('analytics-track');
+        });
+        $('#manualTrack').on('click', function(e){
+            $('#manualTrack').trigger('analytics-track');
+        });
+    }
+
+    bindEvents();
+
+}());
+
+
+//just for require
+if (typeof window.define === "function" && window.define.amd) {
+    define("wiki", [],function() {
+        return _wiki;
+    });
+};
