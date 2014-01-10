8b51601676d01d29d0a024b302842a8d36e5afb2
ross <dl-skycom@bskyb.com>
2014-01-10 16:51:04 +0000

    PM: add missing file

---
+++ _site/_includes/using-the-analytics/default-config/setup.js
@@ -0,0 +1,27 @@
+//page view event
+$('#sessionCamId-page-view-link').on('click', function(e){
+    e.preventDefault();
+    window.sessionCamRecorder = {
+        log :  function(){ },
+        sessionId :  function(){
+            return '987,654';
+        }
+    };
+    analytics.trackPage({
+        site: 'global',
+        section: 'skyglobal/analytics/demo',
+        account: 'bskybdemodev'
+    });
+});
+
+
+//link click event
+$('#sessionCamId-link-click-link').on('click', function(e){
+    e.preventDefault();
+    window.sessionCamRecorder = {
+        log :  function(){ },
+        sessionId :  function(){
+            return '123,343';
+        }
+    };
+});
