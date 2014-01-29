8f76c9b6669019e2cdc44d883e1d3725365411ed
Peter Mouland <peter.mouland@gmail.com>
2014-01-29 15:00:19 +0000

    update debug to stop sending http requests.

--- test/functional/debug.rb
+++ test/functional/debug.rb
@@ -29,4 +29,37 @@ class AnalyticsTest < AcceptanceTest
 
     current_url.must_include '#using-me'
   end
+
+
+  it "Debug can be turned on on page load which displays analytics info within the page" do
+    click_link 'Click here to see debug on load'
+    assert has_css?('#analytics-debug')
+    find('#analytics-debug .pageLoad').text.must_equal 'pageLoad: event1'
+    find('#analytics-debug .pageName').text.must_equal 'pageName: sky/portal/global/skyglobal/analytics/debug/demo'
+    find('#analytics-debug .section2').text.must_equal 'section2: sky/portal/global/skyglobal/analytics/debug'
+  end
+
+  it "Debug can be turned off which will not display analytics info within the page" do
+    click_link 'debug off'
+    click_link 'Standard link'
+    assert has_no_css?('#analytics-debug')
+  end
+
+  it "Debug can be turned on and http requests are blocked" do
+    click_link 'debug off'
+    click_link 'Standard link'
+    trackedEvents.must_equal [eventsMap[:linkClick]]
+    trackedEvents.wont_include eventsMap[:pageLoad]
+
+    click_link 'debug on'
+    click_link 'Click here to see resetting config in action'
+    trackedEvents.must_equal [eventsMap[:linkClick]]
+    trackedEvents.wont_include eventsMap[:pageLoad]
+
+    click_link 'debug off'
+    click_link 'Click here to see resetting config in action'
+
+    trackedEvents.wont_equal [eventsMap[:linkClick]]
+    trackedEvents.must_include eventsMap[:pageLoad]
+  end
 end
