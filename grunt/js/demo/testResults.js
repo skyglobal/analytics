5ba5568dc128994a4bfae434528f5e0a370a003d
joey <dl-skycom@joey.com>
2014-02-14 14:56:26 +0000

    ST|ES: refactored code, added missing files into git

--- grunt/js/demo/testResults.js
+++ grunt/js/demo/testResults.js
@@ -1,72 +1,61 @@
 if (typeof demo==='undefined') demo={};
-var TestResult = {} ||TestResult;
+var TestResult = {} || TestResult;
 
 demo.testResults = (function () {
 
-    var $el = {
-        document : $(document),
-        body : $("body"),
-        testRunLinks : $('.run-test')
-    }
+    var $body = $("body");
+    var $testRunLinks = $('.run-test');
+    var xmlParser = new X2JS();
 
-    function capitalizeFirstLetter (text) {
-        var result = text;
-        if (text) {
-            result = text.toLowerCase().replace(/\b[a-z]/g, function(letter) {
-                return letter.toUpperCase();
-            });
-        }
+    function capitalizeFirstLetter(text) {
+        if (!text) { return ''; }
+        var result = text.toLowerCase().replace(/\b[a-z]/g, function(letter) {
+            return letter.toUpperCase();
+        });
         return result;
     }
+
     function getTestResults(testNamePath) {
         var resultData;
         $.ajax(testNamePath, {async: false}).done(function(data) {
-            resultData = new X2JS().xml2json(data.documentElement);
+            resultData = xmlParser.xml2json(data.documentElement);
         });
         return resultData;
     }
 
-    function renderTestSuite(test) {
-        var html = '',
-        testData = {
-            name: test._name.split(':')[2],
-            errors: +test._failures + +test._errors,
-            tests: +test._tests,
-            time: test._time.substr(0,4)
-        }
+    function renderTestSuite(results) {
+        var data = {
+            name: results._name.split(':')[2],
+            errors: parseInt(results._failures,10) + parseInt(results._errors,10),
+            tests: parseInt(results._tests,10),
+            time: results._time.substr(0,4)
+        };
 
-        html += '<ul id="mocha-stats">'
-        html += '<li class="passes">passes:<em>' +  (testData.tests - testData.errors) +'</em></li>';
-        html += '<li class="failures">failures:<em>' + testData.errors + '</em></li>';
-        html += '<li class="duration">duration:<em>' +  testData.time + 's</em></li>';
-        html += '</ul>';
-        html += '<ul id="mocha-report"><li class="suite"><h1>' + testData.name + '</h1></li></ul>';
-
-       return html;
+        return '<ul id="mocha-stats">' +
+                '<li class="passes">passes:<em>' +  (data.tests - data.errors) +'</em></li>' +
+                '<li class="failures">failures:<em>' + data.errors + '</em></li>' +
+                '<li class="duration">duration:<em>' +  data.time + 's</em></li>' +
+                '</ul>' +
+                '<ul id="mocha-report"><li class="suite"><h1>' + data.name + '</h1></li></ul>';
     }
 
     function renderTestCase(test) {
         var html = '<ul>',
         testData;
-
         $.each(test.testcase, function (index, value) {
             testData = {
                 name: capitalizeFirstLetter(value._name.split('_')[2]),
                 assertions: value._assertions,
                 status: (value.error || value.failure) ? 'fail' : 'pass'
-            }
+            };
             html += '<li class="test fast ' + testData.status + '"><h2>'  + testData.name + '</h2></li>';
         });
         html += '</ul>';
         return html;
     }
 
-    function getTest(TestName) {
-            return getTestResults('test/reports/TEST-AnalyticsTest-'+ TestName +'.xml');
-    }
-
     function createTest(testName) {
-        var testData = getTest(testName),
+        var testData = getTestResults('test/reports/TEST-AnalyticsTest-'+ testName +'.xml'),
         $testLink = $('#'+testName);
 
         if(testData._failures != 0 || testData._errors != 0) {
@@ -79,31 +68,24 @@ demo.testResults = (function () {
         $testLink.lightbox({closeButtonColour: 'black'});
     }
 
-    function bind() {
-        $el.document.ready(function () {
-            var testName;
-           $el.testRunLinks.each(function (index, value) {
-                testName = $(value).attr('data-test-name');
-               var html = '';
-               html += '<article id="test-' + testName + '" class="lightbox-analytics hidden" aria-labelledby="lightbox-demo-link">' +
-                   '<div class="alpha skycom-12">' +
-                        '<h1></h1>' +
-                        '<div class="mocha-container">' +
-                        '</div>' +
-                   '</div>' +
-               '</article>';
-                $el.body.append(html);
-            });
+    function createLightbox(testName){
+        var html = '<article id="test-' + testName + '" class="lightbox-analytics hidden" aria-labelledby="lightbox-demo-link">' +
+            '<div class="alpha skycom-12">' +
+            '<h1></h1>' +
+            '<div class="mocha-container">' +
+            '</div>' +
+            '</div>' +
+            '</article>';
+        $body.append(html);
+    }
 
-           $el.testRunLinks.each(function (index, value) {
-               var testName = $(value).data('test-name');
-               createTest(testName);
-           });
-
+    function createResultsLightboxes(){
+        $testRunLinks.each(function (index, value) {
+            var testName = $(value).attr('data-test-name');
+            createLightbox(testName);
+            createTest(testName);
         });
     }
-    bind();
 
+    createResultsLightboxes();
 })();
-
-
